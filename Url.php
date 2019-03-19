7127a8446163752f7278e8576383a3de2523e241
nikolay921 <nik.finnsailing@gmail.com>
2019-03-19 16:05:19 +0300

    new edited Url.php

--- Url.php
+++ Url.php
@@ -1,83 +1,71 @@
 <?php
-
 /**
  * Interface UrlInterface
  *
  *Provides a set of functions to implement work with Url
  *
  */
-
 interface UrlInterface
 {
     public function getScheme();
     public function getHost();
     public function getQueryParams();
-
 }
-
 /**
  * Class Url
  *
  *implements interface UrlInterface functions
  *
  */
-
 class Url implements UrlInterface
 {
-    public $string;
-    public $string2;
+    public $url;
+    public $scheme;
+    public $host;
+    public $elem;
     public $result = [];
+
 
     public function __construct($url)
     {
         $this->url = $url;
+        $this->scheme = parse_url($this->url, PHP_URL_SCHEME);
+        $this->host = parse_url($this->url, PHP_URL_HOST);
+        $this->elem = parse_url($this->url, PHP_URL_QUERY);
     }
-
     /**
      * defines the protocol
      *
      * @return mixed
      */
-
     public function getScheme()
     {
-        $this->string = parse_url($this->url, PHP_URL_SCHEME);
-        return $this->string;
+        return $this->scheme;
     }
-
     /**
      * will determine their host
      *
      * @return mixed
      */
-
     public function getHost()
     {
-        $this->string = parse_url($this->url, PHP_URL_HOST);
-        return $this->string;
+        return $this->host;
     }
-
-
     /**
      * produces in url variable key and their values
      *
      * @return array
      */
-
     public function getQueryParams()
     {
-        $elem = parse_url($this->url, PHP_URL_QUERY);
-        $elem1 = explode('&', $elem);
+        $elem1 = explode('&', $this->elem);
         foreach($elem1 as $value)
         {
             $array = explode('=', $value);
             $this->result[$array[0]] = $array[1];
         }
-
         return $this->result;
     }
-
-
     /**
      *
      *using the resulting array of the getQueryParams method searches for a specific key
@@ -87,7 +75,6 @@ class Url implements UrlInterface
      *
      * @return mixed|null
      */
-
     public function getQueryParam($key, $default = NULL)
     {
         if(array_key_exists($key, $this->getQueryParams()))
@@ -97,10 +84,9 @@ class Url implements UrlInterface
         {
             $result = $default;
         }
-
         return $result;
     }
 }
-
 $url = new Url('http://yandex.ru?key=value&key2=value2');
+
 
