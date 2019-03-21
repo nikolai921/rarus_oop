3fd21950a332dd398590f018050a5e7b56f78cb1
nikolai921 <nik.finnsailing@gmail.com>
2019-03-21 06:12:42 +0300

    Add new file Url.php

--- Url.php
+++ Url.php
@@ -1,30 +1,101 @@
 <?php
 
+declare(strict_types=1);
+
 require_once('UrlInterface.php');
 
 /**
- * Class Url
+ * Class Url наследует методы Интерфейса  UrlInterface, и реализует представления параметров сущности Url.
+ * На  вход принимается данные строгой типизации строки, непосредственно сам Url.
+ * Методом __construct, производится проверка входных данных и разборка строки на основные параметры.
+ * В результате чего производится присвоение полученных значений основным свойствам класса,
+ * которые закрыты для внешнего использования, и доступны для потомков класса.
+ *
+ * Основные свойства:
+ *
+ * $componentUrl - массив состоящий из всех возможных элементов Url;
+ * $inputParam - входная строка;
+ * $schemeUrl;
+ * $hostUrl;
+ * $portUrl;
+ * $userUrl;
+ * $passUrl;
+ * $pathUrl;
+ * $fragmentUrl;
+ * $queryParam;
+ *
+ * Реализована полноценная разборка Url, что позволяет расширять данный класс, другими методами в потомках.
+ *
+ * Для обращения к данным свойствам создан набор методов, которые отвечают за представления данных свойств вне класса.
+ * В основе закладывалась реализация, представления отдельных элементов строки Url,
+ * что является единственной обязанностью класса Url.
  */
 
 class Url implements UrlInterface
 {
-    private $componentUrl;
-    private $inputParam;
-    private $schemeUrl;
-    private $hostUrl;
-    private $queryParam;
-    private $selectionParam = [];
+    protected $componentUrl = 'Элемент отсутствует';
+    protected $inputParam = 'Элемент отсутствует';
+    protected $schemeUrl = 'Элемент отсутствует';
+    protected $hostUrl = 'Элемент отсутствует';
+    protected $portUrl = 'Элемент отсутствует';
+    protected $userUrl = 'Элемент отсутствует';
+    protected $passUrl = 'Элемент отсутствует';
+    protected $pathUrl = 'Элемент отсутствует';
+    protected $fragmentUrl = 'Элемент отсутствует';
+    protected $queryParam = 'Элемент отсутствует';
+    protected $paramSet = [];
 
 
-    public function __construct($url)
+    public function __construct(string $url)
     {
-        if(filter_var($url, FILTER_VALIDATE_URL))
+        if(filter_var($url, FILTER_VALIDATE_URL) !== false)
         {
             $this->inputParam = $url;
+
             $this->componentUrl = parse_url($this->inputParam);
-            $this->schemeUrl = $this->componentUrl['sheme'];
-            $this->hostUrl = $this->componentUrl['host'];
-            $this->queryParam = $this->componentUrl['query'];
+
+            if(!empty($this->componentUrl['scheme']))
+            {
+                $this->schemeUrl = $this->componentUrl['scheme'];
+            }
+
+            if(!empty($this->componentUrl['host']))
+            {
+                $this->hostUrl = $this->componentUrl['host'];
+            }
+
+            if(!empty($this->componentUrl['port']))
+            {
+                $this->hostUrl = $this->componentUrl['port'];
+            }
+
+            if(!empty($this->componentUrl['pass']))
+            {
+                $this->hostUrl = $this->componentUrl['pass'];
+            }
+
+            if(!empty($this->componentUrl['path']))
+            {
+                $this->hostUrl = $this->componentUrl['path'];
+            }
+
+            if(!empty($this->componentUrl['user']))
+            {
+                $this->hostUrl = $this->componentUrl['user'];
+            }
+
+            if(!empty($this->componentUrl['query']))
+            {
+                $this->queryParam = $this->componentUrl['query'];
+
+                $interimVar = explode('&', $this->queryParam);
+                foreach($interimVar as $value)
+                {
+                    $interimValue = explode('=', $value);
+                    $this->paramSet[$interimValue[0]] = $interimValue[1];
+                }
+            }
+
         } else{
             throw new Exception('Введенный адрес не прошел фильтрацию');
         }
@@ -42,20 +113,14 @@ class Url implements UrlInterface
 
     public function getQueryParams()
     {
-        $interimVar = explode('&', $this->queryParam);
-        foreach($interimVar as $value)
-        {
-            $interimValue = explode('=', $value);
-            $this->selectionParam[$interimValue[0]] = $interimValue[1];
-        }
-        return $this->selectionParam;
+        return $this->paramSet;
     }
 
-    public function getQueryParam($key, $default = NULL)
+    public function getQueryParam(string $key, string $default = NULL) : string
     {
-        if(array_key_exists($key, $this->getQueryParams()))
+        if(!empty($this->paramSet) && array_key_exists($key, $this->paramSet))
         {
-            $selectionParam = $this->getQueryParams()[$key];
+            $selectionParam = $this->paramSet[$key];
         } else
         {
             $selectionParam = $default;
@@ -65,5 +130,5 @@ class Url implements UrlInterface
 }
 $url = new Url('http://yandex.ru?key=value&key2=value2');
 
-print_r($url->getScheme());
+print_r($url->getQueryParam('new', 'ehu'));
 
